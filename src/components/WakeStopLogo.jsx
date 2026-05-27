e41f7b7d07d1981eccbff55e1dacb67b1568a1b3
Matt Smith <matt@mtsart.com>
2026-05-27 05:28:17 +0000

    File changes

--- src/components/WakeStopLogo.jsx
+++ src/components/WakeStopLogo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LOGO_URL = 'https://media.base44.com/images/public/6a1671fb50d588b068e1ab55/7c9268b8d_hf_20260527_042941_95796515-1dd5-4d2e-b95a-6fac80ee2ffa.png';
+const LOGO_URL = 'https://media.base44.com/images/public/6a1671fb50d588b068e1ab55/15263a1b1_wakestop.png';
 
 export default function WakeStopLogo({ size = 40, className = '' }) {
   return (
